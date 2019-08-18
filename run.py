a1ba1fc6fce28f4dc1a6e78aee5ece005c8b5aeb
moHammad mazaheri <msaxmsa@gmail.com>
2019-08-18 19:48:12 +0430

    go to instagram

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 from selenium import webdriver
 import time 
 from selenium.webdriver.common.keys import Keys
+
+driver = webdriver.Chrome()
+# driver.get('https://www.instagram.com/')
+# driver.find_element_by_xpath('//a[@href="/accounts/login/?source=auth_switcher"]').send_keys(Keys.ENTER)
+# input('go to page login')
+driver.get('https://www.instagram.com/accounts/login/?source=auth_switcher')
